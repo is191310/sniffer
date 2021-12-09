eb36c7b7651e29a83be25154eeb19bd5b558d352
Max <is191303@fhstp.ac.at>
2021-12-09 09:02:10 +0100

    repo

---
+++ src/main/java/at/ac/fhstp/sniffer/repository/SnifferRepository.java
@@ -0,0 +1,11 @@
+package at.ac.fhstp.sniffer.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import at.ac.fhstp.sniffer.Entity.Sniffer;
+
+
+public interface SnifferRepository extends CrudRepository <Sniffer, Integer>
+{
+   
+}
