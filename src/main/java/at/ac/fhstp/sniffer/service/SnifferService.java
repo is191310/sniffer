bd647ca5b5b8f90a8dad6ba694b881c5879c1092
Max <is191303@fhstp.ac.at>
2021-12-09 12:22:25 +0100

    Created Services

---
+++ src/main/java/at/ac/fhstp/sniffer/service/SnifferService.java
@@ -0,0 +1,34 @@
+package at.ac.fhstp.sniffer.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import at.ac.fhstp.sniffer.Entity.Sniffer;
+import at.ac.fhstp.sniffer.repository.SnifferRepository;
+
+@Service
+public class SnifferService {
+    SnifferRepository snifferRepository;
+
+    @Autowired
+    public List<Sniffer> getAllSniffers() {
+        List<Sniffer> sniffers = new ArrayList<Sniffer>();
+        snifferRepository.findAll().forEach(sniffer -> sniffers.add(sniffer));
+        return sniffers;
+    }
+
+    public Sniffer getSnifferbyId(int id) {
+        return snifferRepository.findById(id).get();
+    }
+
+    public void saveOrUpdate(Sniffer sniffer) {
+        snifferRepository.save(sniffer);
+    }
+
+    public void delete(int id)
+    {
+        snifferRepository.deleteById(id);
+    }
+}
