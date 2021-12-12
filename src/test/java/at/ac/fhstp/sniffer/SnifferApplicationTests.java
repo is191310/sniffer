b7567ac8c984a4fcb50a53b33a089ca34f2fe0f1
Max <is191303@fhstp.ac.at>
2021-12-12 23:57:51 +0100

    /sniffer Test status

--- src/test/java/at/ac/fhstp/sniffer/SnifferApplicationTests.java
+++ src/test/java/at/ac/fhstp/sniffer/SnifferApplicationTests.java
@@ -1,13 +1,29 @@
 package at.ac.fhstp.sniffer;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import at.ac.fhstp.sniffer.repository.SnifferRepository;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SnifferApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	SnifferRepository snifferRepository;
+
 	@Test
-	void contextLoads() {
+	void shouldReturnSniffers() throws Exception {
+		this.mockMvc.perform(get("/sniffer"))
+				.andDo(print())
+				.andExpect(status().isOk());
 	}
 
 }
