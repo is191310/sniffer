b4d78db8e2f4da36dda9bfd363431a10bf4b4945
Max <is191303@fhstp.ac.at>
2021-12-13 01:07:38 +0100

    comments and pubdate

--- src/test/java/at/ac/fhstp/sniffer/SnifferApplicationTests.java
+++ src/test/java/at/ac/fhstp/sniffer/SnifferApplicationTests.java
@@ -4,11 +4,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.test.web.servlet.MockMvc;
-import at.ac.fhstp.sniffer.repository.SnifferRepository;
+
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,7 +20,6 @@ class SnifferApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
 
-	SnifferRepository snifferRepository;
 
 	@Test
 	void shouldReturnSniffers() throws Exception {
@@ -26,4 +28,17 @@ class SnifferApplicationTests {
 				.andExpect(status().isOk());
 	}
 
+	@Test
+	void shouldReturnPubdate() throws Exception {
+		this.mockMvc.perform(get("/pubdate"))
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
+	@Test
+	void shouldReturnComment() throws Exception {
+		this.mockMvc.perform(get("/comment"))
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
+	
 }
